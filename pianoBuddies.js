f538f92adc2983e270eca0b45f459fcc88e7d903
Cristian Lopez <cristain_lopez@mail.fresnostate.edu>
2019-12-22 22:56:04 -0800

    added required files

---
+++ pianoBuddies.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+    var piano=new Keyboard();
+    piano.init();
+};
+Keyboard.prototype={
+    init: function(){
+        var keyboard=document.getElementById('piano');
+
+
+
+
+
+
+
+    }
+}
